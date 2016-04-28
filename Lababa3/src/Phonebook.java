9a63fb480310210bcf36a4625289cd9ba7c512d9
greyfox2208 <valeriyaoleneva@yandex.ru>
2016-04-29 01:15:08 +0300

    Add text file in git. Can copy file 1 and write in file 2.

--- Lababa3/src/Phonebook.java
+++ Lababa3/src/Phonebook.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Created by valer on 29.04.2016.
@@ -14,7 +12,30 @@ public class Phonebook {
         System.out.println("Hello!");
         String str = getTex(PATH_READ);
         System.out.println(str);
+        String str2 = getTex(PATH_WRITE);
+        System.out.println(str2);
+        writeText(PATH_WRITE, str);
+        str2 = getTex(PATH_WRITE);
+        System.out.println(str2);
     }
+
+    private static void writeText(String path, String text) {
+        File file = new File(path);
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            PrintWriter out = new PrintWriter(file.getAbsoluteFile());
+            try {
+                out.println(text);
+            } finally {
+                out.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
         public static String getTex(String path) {
             StringBuilder str = new StringBuilder();
